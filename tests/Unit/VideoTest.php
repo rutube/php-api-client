819b09a98c30a59a8d56f2f0d8e968d11ab209bd
Vadim Madison <vmadison@rutube.ru>
2015-02-19 18:46:10 +0300

    license notice

--- tests/Unit/VideoTest.php
+++ tests/Unit/VideoTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Rutube PHP API Client package.
+ *
+ * (c) Rutube
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 include_once('BaseTest.php');
 
 /**
