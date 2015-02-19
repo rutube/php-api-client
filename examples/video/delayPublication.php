15fe135852f566a11a401f055aee9d407040262c
Vadim Madison <vmadison@rutube.ru>
2015-02-19 11:59:38 +0300

    examples fix

--- examples/video/delayPublication.php
+++ examples/video/delayPublication.php
@@ -6,8 +6,11 @@ require_once __DIR__ . '/../../vendor/autoload.php';
  **/
 $rutube = new \Rutube\Rutube('squirrel@squirrel.ru', '998800', /*secure*/ true);
 
+// объект видео
+$video = $rutube->video();
+
 // загрузить свои видео
-$video = $rutube->video()->upload('http://pic.rutube.ru/staticfile/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
+$data = $video->upload('http://pic.rutube.ru/staticfile/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
 
 // отложить публикацию
 $status = $video->publication($data->video_id, '2015-01-16 20:36:31');
