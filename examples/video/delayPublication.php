e3006268a14d18941536bb07407d43cd4343050b
Vadim Madison <vmadison@rutube.ru>
2015-02-19 11:52:45 +0300

    new url to video file in examples

--- examples/video/delayPublication.php
+++ examples/video/delayPublication.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $rutube = new \Rutube\Rutube('squirrel@squirrel.ru', '998800', /*secure*/ true);
 
 // загрузить свои видео
-$video = $rutube->video()->upload('http://dl.dropboxusercontent.com/u/17497896/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
+$video = $rutube->video()->upload('http://pic.rutube.ru/staticfile/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
 
 // отложить публикацию
 $status = $video->publication($data->video_id, '2015-01-16 20:36:31');
--- examples/video/getVideo.php
+++ examples/video/getVideo.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $rutube = new \Rutube\Rutube('squirrel@squirrel.ru', '998800', /*secure*/ true);
 
 // загрузить свои видео
-$data = $rutube->video()->upload('http://dl.dropboxusercontent.com/u/17497896/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
+$data = $rutube->video()->upload('http://pic.rutube.ru/staticfile/27808763b94d4e479e6ed98fe9e54ec2.mp4', 'title', 'description', 1,13);
 
 // Получить видео
 $v = $rutube->video()->getVideo($data->video_id);
