da5f650e6d50ce28a2d1df68a560aab596a0d4b1
Vadim Madison <vmadison@rutube.ru>
2015-05-08 13:01:14 +0300

    extended authorization example

--- examples/authorization.php
+++ examples/authorization.php
@@ -1,10 +1,20 @@
 <?php
 
+use Rutube\Exceptions;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
 /**
  * Авторизация
  **/
-$rutube = new \Rutube\Rutube('squirrel@squirrel.ru', '998800', /*secure*/ true);
+try {
+    $rutube = new \Rutube\Rutube('squirrel@squirrel.ru', '99880011', /*secure*/ true);
 
-echo "Авторизован: " . $rutube->isAuthorized()."\n"; //result: true/false
-echo "Защищенное:  " . $rutube->isSecure()."\n";     // Возвращает тип соединения. В случае https - true; в случае http - false
+    echo "Авторизован: " . ($rutube->isAuthorized() ? "да" : "нет") . "\n"; // result: true/false
+    echo "Соединение:  " . ($rutube->isSecure() ? "https" : "http") . "\n"; // Возвращает тип соединения. В случае https - true; в случае http - false
+
+} catch (Exceptions\ConnectionErrorException $ex) {
+    echo "ConnectionErrorException: Ошибка доступа к Rutube\n";
+} catch (Exceptions\BadRequestException $ex) {
+    echo "BadRequestException: Некорректные параметры авторизации\n";
+}
