bf94129907505a82cbaf34401d318c6f1a2901a6
trong <vadim.a.madison@gmail.com>
2015-07-03 16:28:24 +0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -14,7 +14,7 @@
  * Мы, все же, рекомендуем использовать composer - https://getcomposer.org/
  */
 
-if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     throw new Exception('The Rutube API Client requires PHP version 5.4 or higher.');
 }
 
